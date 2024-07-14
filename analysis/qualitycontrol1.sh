3118f45383b03f187832991c3c7721edfc13a8e8
hcouliba <131803241+HawaBioinformatics@users.noreply.github.com>
2024-07-14 13:00:49 -0400

    Update qualitycontrol1.sh

--- analysis/qualitycontrol1.sh
+++ analysis/qualitycontrol1.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# INPUT : Raw FastQ files
+# OUTPUT : FastQC reports in HTML format
+
 mkdir qc_reports1
 
 fastqc /Users/hawacoulibaly/Downloads/FASTQfiles/FastQfiles/*.fastq -o qc_reports1/
-
 
