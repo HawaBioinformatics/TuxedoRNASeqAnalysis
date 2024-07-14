2ccc24716a76544ddbdfad302bfac82a0bca90a5
hcouliba <131803241+HawaBioinformatics@users.noreply.github.com>
2024-07-14 13:26:49 -0400

    Update qualitycontrol1.sh

--- analysis/qualitycontrol1.sh
+++ analysis/qualitycontrol1.sh
@@ -3,7 +3,17 @@
 # INPUT : Raw FastQ files
 # OUTPUT : FastQC reports in HTML format
 
-mkdir qc_reports1
+# this command creates a directory for storing results
+# be sure to inlcude the path and name where the file should be stored
 
-fastqc /Users/hawacoulibaly/Downloads/FASTQfiles/FastQfiles/*.fastq -o qc_reports1/
+mkdir /Users/hawacoulibaly/Documents/GitHub/TuxedoRNASeqAnalysis/output/FastQC_report
+
+# this command excecutes the FastQC anlysis 
+# the first path in this command indicates the fastq files used as input
+# the second path indicates the output directory created in the previous command
+
+fastqc /Users/hawacoulibaly/Downloads/FASTQfiles/FastQfiles/*.fastq -o /Users/hawacoulibaly/Documents/GitHub/TuxedoRNASeqAnalysis/output/FastQC_report/
+
+# remove unwanted/duplicated files (if necessary)
+rm *.zip
 
