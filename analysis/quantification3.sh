c7ba9530b322d4cdca63ed8adabbb237903b0565
hcouliba <131803241+HawaBioinformatics@users.noreply.github.com>
2024-07-15 11:55:12 -0400

    Update quantification3.sh

--- analysis/quantification3.sh
+++ analysis/quantification3.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# install brew 
+# eval "$(/opt/homebrew/bin/brew shellenv)"
 
+# https://github.com/signalbash/how_are_we_stranded_here
 # create a directory for salmon quantification
 mkdir /Users/hawacoulibaly/Documents/GitHub/TuxedoRNASeqAnalysis/output/salmon_quant
 
@@ -21,8 +24,11 @@ salmon index -t transcripts.fa -i transcripts_index
 # output directory
 cd /Users/hawacoulibaly/Documents/GitHub/TuxedoRNASeqAnalysis/output/salmon_quant
 
+# Python package for testing strandedness of RNA-Seq fastq files
+# check_strandedness --gtf /Users/hawacoulibaly/Downloads/TestFiles/gencode.v38.annotation.gtf --transcripts /Users/hawacoulibaly/Downloads/TestFiles/transcripts.fa --reads_1 /Users/hawacoulibaly/Downloads/TestFiles/SRR1027185_1.fastq --reads_2 /Users/hawacoulibaly/Downloads/TestFiles/SRR1027185_2.fastq
+
 # Run Salmon quantification for paired-end reads
 salmon quant -i /Users/hawacoulibaly/Downloads/TestFiles/transcripts_index -l A \
   -1 /Users/hawacoulibaly/Downloads/TestFiles/SRR1027185_1.fastq  -2 /Users/hawacoulibaly/Downloads/TestFiles/SRR1027185_2.fastq \
-  -p 8 -o SRR1027185_salmon_output
+  -p 8 --validateMappings -o SRR1027185_salmon_output
 
