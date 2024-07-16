ca29f6ab449a009ece151e1e48dc8281e69ff145
hcouliba <131803241+HawaBioinformatics@users.noreply.github.com>
2024-07-15 20:53:45 -0400

    Update quantification3.sh

--- analysis/quantification3.sh
+++ analysis/quantification3.sh
@@ -9,14 +9,14 @@ mkdir /Users/hawacoulibaly/Documents/GitHub/TuxedoRNASeqAnalysis/output/salmon_q
 
 # download the reference transcriptome
 
-# wget ftp://ftp.ensembl.org/pub/release-101/fasta/homo_sapiens/cdna/Homo_sapiens.GRCh38.cdna.all.fa.gz
-# gunzip Homo_sapiens.GRCh38.cdna.all.fa.gz
+# https://www.gencodegenes.org/human/release_46.html
+# gunzip gencode.v46.transcripts.fa.gz
 
 # transcriptome reference directory
 # cd /Users/hawacoulibaly/Downloads/TestFiles
 
 # rename reference genome file
-# mv Homo_sapiens.GRCh38.cdna.all.fa transcripts.fa
+# mv gencode.v46.transcripts.fa transcripts.fa
 
 # Create Salmon index
 salmon index -t transcripts.fa -i transcripts_index
@@ -25,10 +25,13 @@ salmon index -t transcripts.fa -i transcripts_index
 cd /Users/hawacoulibaly/Documents/GitHub/TuxedoRNASeqAnalysis/output/salmon_quant
 
 # Python package for testing strandedness of RNA-Seq fastq files
+# https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-022-04572-7
 # check_strandedness --gtf /Users/hawacoulibaly/Downloads/TestFiles/gencode.v38.annotation.gtf --transcripts /Users/hawacoulibaly/Downloads/TestFiles/transcripts.fa --reads_1 /Users/hawacoulibaly/Downloads/TestFiles/SRR1027185_1.fastq --reads_2 /Users/hawacoulibaly/Downloads/TestFiles/SRR1027185_2.fastq
 
 # Run Salmon quantification for paired-end reads
+# Library type: IU (an unstranded paired-end library where the reads face each other)
+
 salmon quant -i /Users/hawacoulibaly/Downloads/TestFiles/transcripts_index -l A \
-  -1 /Users/hawacoulibaly/Downloads/TestFiles/SRR1027185_1.fastq  -2 /Users/hawacoulibaly/Downloads/TestFiles/SRR1027185_2.fastq \
-  -p 8 --validateMappings -o SRR1027185_salmon_output
+  -1 /Users/hawacoulibaly/Downloads/TestFiles/SRR1027186_1.fastq  -2 /Users/hawacoulibaly/Downloads/TestFiles/SRR1027186_2.fastq \
+  -p 8 --validateMappings --outFilterMultimapNmax -o SRR1027186_salmon_output
 
